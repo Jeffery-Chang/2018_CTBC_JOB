99c80f1258c9d3f4e12491ed51bf2ef179d70581
Unknown <hn85412404@gmail.com>
2018-03-06 00:54:11 +0800

    新增次頁+nav click function

--- js/init.js
+++ js/init.js
@@ -1,5 +1,14 @@
 $(function() {
     $("#slider").slider();
 
+    $('.indexPage').on('click', 'li.inactive', function() {
+
+    })
+
 
 });
+
+function changePage(ele, param) {
+    console.log(ele);
+    window.location = ele + ".html" + param;
+}
