d10df6a11fc2fa7dfc113f2cc5a54e0bcd8ea02a
jeffery_chang <jeffery_chang@webgene.com.tw>
2018-03-08 14:07:56 +0800

    2018-03-08 fixed funds slide()

--- js/funds.js
+++ js/funds.js
@@ -3,7 +3,15 @@ var fundsCtrl = {
         $('#top .indexPage li:eq(1)').click(function(e){
             e.preventDefault();
             trackWaitJump('', 'index.html?from=1');
-        })
+        });
+
+        $('#funds_area .f_black').click(function(e){
+            e.preventDefault();
+            if(!menuCtrl.chkDevice() || $(window).width() >= 400) return;
+            $(this).parent('li').toggleClass('show');
+            $(this).parent('li').find('.fund_detail').slideToggle();
+            $(this).parent('li').find('.t_center').slideToggle();
+        });
     }
 }
 
